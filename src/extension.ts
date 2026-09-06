d113f5fa1999211ada8c3c40e5de202b05aa3d26
laofeng <laofyx@163.com>
2026-09-07 00:19:03 +0800

    fix: Open the log window by default

--- src/extension.ts
+++ src/extension.ts
@@ -7,8 +7,6 @@ export function activate(context: vscode.ExtensionContext) {
   // ===== 初始化日志 =====
   logger.initialize()
   config.initialize()
-  // 显示日志面板
-  logger.show()
 
   // ===== 注册命令 =====
   const command = registerCommands(context)
